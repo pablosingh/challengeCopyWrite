f0ac97a42f52626879cdbfdb5d65925bfafb9f96
Singh Pablo <pablo22_4@hotmail.com>
2022-07-27 21:59:54 -0300

    Creacion de Api y Client

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import { Body } from './components/Body';
+import { Head } from './components/Head';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Container from '@material-ui/core/Container'
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Container>
+        <div className="App">
+          <Head /> 
+          <Body/> 
+        </div>
+      </Container>
+    </Provider>
+  );
+}
+
+export default App;
